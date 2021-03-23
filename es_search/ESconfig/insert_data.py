006dc10eab17cfc440fdfbc21dc37dcbe8a0d2d1
Mir Sameed Ali <meersameed@gmail.com>
2021-03-23 11:27:09 +0530

    Added script to Push data to es server

---
+++ es_search/ESconfig/insert_data.py
@@ -0,0 +1,31 @@
+from elasticsearch import Elasticsearch, helpers
+import csv
+import json
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+print(es)
+
+
+# def csv_reader_row_wise(file_obj, doc_name):
+#     reader = csv.DictReader(file_obj)
+#     i = 1
+
+#     for row in reader:
+#         print(i)
+#         es.index(index='answerbot', doc_type=doc_name, id=i,
+#                  body=json.dumps(row))
+#         i = i + 1
+#         # print(row)
+#         if i == 500:
+#             break
+
+
+def csv_reader(file_obj, index_name, doc_name):
+    reader = csv.DictReader(file_obj)
+    helpers.bulk(es, reader, index=index_name, doc_type=doc_name)
+
+
+if __name__ == "__main__":
+    with open("../../Data/questions_data.csv") as f_obj:
+        csv_reader(f_obj, "answerbot_questions", "questions")
+    with open("../../Data/answers_data_new.csv") as f_obj:
+        csv_reader(f_obj, "answerbot_answers", "answers")
