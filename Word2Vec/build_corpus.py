2f95c5a3d689229b4b89635d6e495f3511167075
officiallynik <nikhilownstheworld@gmail.com>
2021-03-23 18:49:53 +0530

    modified word2vec

--- Word2Vec/build_corpus.py
+++ Word2Vec/build_corpus.py
@@ -1,37 +1,35 @@
-import multiprocessing as mp
 import sys
 sys.path.append("../PreProcessor")
 sys.path.append("../Data")
 import pandas as pd
 from preprocessor import PreprocessPostContent
 
-def process_data(data, cnt):
+def process_data(data):
+    # print("processing...")
     preprocessor = PreprocessPostContent()
     title = preprocessor.get_single_para_word_list(data[0])
     body = preprocessor.get_mul_para_wordlist_list(data[1])
-    
-    if (cnt + 1)%10000 == 0:
-        print(f"processed: {cnt} data items")
     
     return title, body
 
 def create_corpus():
     dataset = pd.read_csv("../Data/questions_data.csv")
-    dataset = zip(dataset[:10]['title'], dataset[:10]['body'])
 
-    pool = mp.Pool(mp.cpu_count())
+    for i in range(50000, 300000, 10000):
+        datasubset = zip(dataset[i:i+10000]['title'], dataset[i:i+10000]['body'])
 
-    print("starting to process dataset")
-    data_list = [pool.apply(process_data, args=(data, cnt)) for cnt, data in enumerate(dataset)]
-    # print(data_list)
+        data_list = []
+        print("starting to process dataset")
+        for data in datasubset:
+            data_list.append(process_data(data))
 
-    print("starting to add data to txt file")
+        print("starting to add data to txt file")
 
-    with open("corpus.txt", 'w', encoding="utf-8") as f:
-        for data in data_list:
-            f.write(f"{data[0]}\n{data[1]}\n")
+        with open("corpus.txt", 'a', encoding="utf-8") as f:
+            for data in data_list:
+                f.write(f"{data[0]}\n{data[1]}\n")
 
-    print("successfully wrote data to the csv file")
+        print(f"successfully wrote data to the csv file, completed: {i+10000} data items in total")
 
 if __name__ == '__main__':
     create_corpus()
