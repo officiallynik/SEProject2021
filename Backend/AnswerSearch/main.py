efd3a971c9ea5edec3578504180259bb3628d24c
Mir Sameed Ali <meersameed@gmail.com>
2021-04-07 20:43:55 +0530

    Added query function in main.py

--- Backend/AnswerSearch/main.py
+++ Backend/AnswerSearch/main.py
@@ -7,6 +7,7 @@ from answersretrieval import retrieve_top_matched_answers
 from summarizer import mmr_handler
 from html.parser import HTMLParser
 import re
+import json
 mmr=mmr_handler()
 
 class MyHTMLParser(HTMLParser):
@@ -27,23 +28,53 @@ def answer_display(answer_raw):
     parserHTML.feed(processed_answer)
 
 
-while True:
-    inp = input("query > ")
-    if inp == "exit":
-        break
-    inp = inp.split("--")
-    query = inp[0]
+def get_answer_summary(query):
     
-    questions = retrieve_top_matched_questions(query, 5, len(inp)>1)
+    questions = retrieve_top_matched_questions(query, 5, False)
     
     sorted_paragraphs=retrieve_top_matched_answers(questions, query)
     final_paragraphs=mmr.rank_mmr(sorted_paragraphs)
-    #print(final_paragraphs)
-    print("\n\n")
+
+    response_paragraphs=[]
+
     for cnt, para_obj in enumerate(final_paragraphs, 1):
-        #print(f"answer no. {cnt}")
-        #print("\nanswer:")
+            #print(f"answer no. {cnt}")
+            #print("\nanswer:")
+
+            response_paragraphs.append(para_obj["paragraph"].raw_text)
+            #print("MMR Score: ",para_obj["mmr_score"])
+            #print("--------------------------------------------------------------------------------------------")
+
+
+    respose={"questions":questions,"answers":response_paragraphs}
+
+    print(respose)
+    json_res=json.dumps(respose)
+    return json_res
     
-        answer_display(para_obj["paragraph"].raw_text)
-        #print("MMR Score: ",para_obj["mmr_score"])
-        print("--------------------------------------------------------------------------------------------")
+
+
+
+
+if __name__=="__main__":
+    get_answer_summary("print in python")
+    while True:
+        inp = input("query > ")
+        if inp == "exit":
+            break
+        inp = inp.split("--")
+        query = inp[0]
+
+        questions = retrieve_top_matched_questions(query, 5, len(inp)>1)
+
+        sorted_paragraphs=retrieve_top_matched_answers(questions, query)
+        final_paragraphs=mmr.rank_mmr(sorted_paragraphs)
+        #print(final_paragraphs)
+        print("\n\n")
+        for cnt, para_obj in enumerate(final_paragraphs, 1):
+            #print(f"answer no. {cnt}")
+            #print("\nanswer:")
+
+            answer_display(para_obj["paragraph"].raw_text)
+            #print("MMR Score: ",para_obj["mmr_score"])
+            print("--------------------------------------------------------------------------------------------")
