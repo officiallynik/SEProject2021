b7a6c2841e3ee776ed99537259b8d88c212c8ae9
Mir Sameed Ali <meersameed@gmail.com>
2021-04-22 02:18:38 +0530

    RCA.py

--- Backend/AnswerSearch/AnswersRetrieval/RCA.py
+++ Backend/AnswerSearch/AnswersRetrieval/RCA.py
@@ -6,20 +6,42 @@ from gensim.models import Word2Vec
 
 
 class RCA:
+    """
+    Contains relevant methods for relevance calculation
+
+   """
     def get_idf_metrics(self):
+
+        """
+        Returns a dictionary of words with their idf scores.
+        
+        Parameters:
+        None
+
+        Returns:
+        Dictionary: words with their idf scores
+            
+        """
         idf_metric_dict = {}
         with open("./IDF/idf.csv", encoding="utf8") as f:
             read_csv = csv.reader(f, delimiter=',')
-            # print("loading idf metrics")
             for row in read_csv:
                 idf_metric_dict[row[0]] = row[1]
 
         return idf_metric_dict
 
     def get_word2vec_model(self):
-        # print("loading word2vec model")
+        """
+        Returns the trained Word2Vec Model 
+
+        Parameters:
+        None
+
+        Returns:
+        Trained Model
+            
+        """
         model = Word2Vec.load("./Word2Vec/word2vec.model")
-        # print("model loaded successfully")
         return model
 
     def __init__(self):
@@ -27,6 +49,16 @@ class RCA:
         self.word2Vec = self.get_word2vec_model()
 
     def calc_asymmetric_val(self, query_list, Question_List):
+         """
+        Returns the calculated asymmetric relevance score between passed query_list and Question_list
+        
+        Parameters:
+        query_list, Question_List (list): List of words 
+
+        Returns:
+        float: relevance score
+            
+        """
         rel_idf_summation = []
         idf_values = []
 
@@ -59,11 +91,19 @@ class RCA:
         return asymmetric_rel
 
     def calc_symmetric_relevance(self, query_list, Question_List):
-        print("hello")
+        """
+        Returns the calculated symmetric relevance score between passed query_list and Question_list
+        
+        Parameters:
+        query_list, Question_List (list): List of words 
+
+        Returns:
+        float: relevance score
+            
+        """
         rel_q_to_Q = self.calc_asymmetric_val(query_list, Question_List)
         rel_Q_to_q = self.calc_asymmetric_val(Question_List, query_list)
         average_relevance = (rel_q_to_Q + rel_Q_to_q) / 2
-        print(average_relevance)
         return average_relevance
 
 if __name__ == "__main__":
